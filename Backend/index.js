d98a053205e1ba9aa95542dbeae9610053e21685
Gustavo Fernandes <goncalves_017@hotmail.com>
2024-02-26 18:46:39 -0300

    :sparkles:First commit

---
+++ Backend/index.js
@@ -0,0 +1,81 @@
+import puppeteer from 'puppeteer';
+import express from 'express';
+import cors from 'cors';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+// const keyword = 'harry potter';
+
+app.post('/api/scraped', async (req, res) => {
+  const keyword = req.body.keyword;
+  const url = `https://www.amazon.com.br/s?k=${keyword}`;
+  console.log(keyword);
+  console.log(url);
+  try {
+    const browser = await puppeteer.launch({
+      headless: true, // Change to true for production
+      defaultViewport: null,
+    });
+    const page = await browser.newPage();
+
+    await page.goto(url, {
+      waitUntil: 'domcontentloaded',
+    });
+
+    await page.waitForSelector('[data-component-type="s-search-result"]');
+
+    const products = await page.evaluate(() => {
+      const productElements = document.querySelectorAll(
+        '[data-component-type="s-search-result"]'
+      );
+      const productsArray = [];
+
+      productElements.forEach(element => {
+        const obj = { title: '', rating: '', numberOfReviews: '', img: '' };
+
+        const productTitle = element.querySelector('h2 span');
+        obj.title = productTitle ? productTitle.textContent : 'N/A';
+
+        const numberReviewsElement = element.querySelector(
+          '.a-size-base.s-underline-text'
+        );
+        obj.numberOfReviews = numberReviewsElement
+          ? Number(numberReviewsElement.textContent)
+          : 'N/A';
+
+        const ratingElement = element.querySelector('.a-icon-alt');
+        obj.rating = ratingElement
+          ? Number(ratingElement.textContent.split(' ')[0].replace(',', '.'))
+          : 'N/A';
+
+        const imgElement = element.querySelector('.s-image');
+        obj.img = imgElement ? imgElement.getAttribute('src') : 'N/A';
+
+        productsArray.push(obj);
+      });
+
+      return productsArray;
+    });
+
+    await browser.close();
+
+    res.json(products);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(PORT, () => console.log(`Server is listening to port ${PORT}`));
---
+++ Frontend/script.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const searchInput = document.getElementById('searchInput');
+  const searchBtn = document.getElementById('searchBtn');
+  const resultContainer = document.getElementById('resultContainer');
+
+  searchBtn.addEventListener('click', async () => {
+    const searchedWord = searchInput.value.trim();
+
+    if (searchedWord) {
+      try {
+        const response = await fetch('/api/scraped', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ keyword: searchedWord }),
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+
+        const data = await response.json();
+        displayResults(data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    } else {
+      alert('Please enter a search term.');
+    }
+  });
+
+  function displayResults(products) {
+    resultContainer.innerHTML = '';
+
+    products.forEach(product => {
+      const productDiv = document.createElement('div');
+      productDiv.classList.add('product');
+
+      const img = document.createElement('img');
+      img.src = product.img;
+      img.alt = 'Product Image';
+      productDiv.appendChild(img);
+
+      const title = document.createElement('h3');
+      title.textContent = product.title;
+      productDiv.appendChild(title);
+
+      const rating = document.createElement('p');
+      rating.textContent = `Rating: ${product.rating}`;
+      productDiv.appendChild(rating);
+
+      const numReviews = document.createElement('p');
+      numReviews.textContent = `Number of Reviews: ${product.numberOfReviews}`;
+      productDiv.appendChild(numReviews);
+
+      resultContainer.appendChild(productDiv);
+    });
+  }
+});
