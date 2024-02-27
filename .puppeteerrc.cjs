0da0bb243d798e8922a5b39b56b09b7c6c305d1e
Gustavo Fernandes <goncalves_017@hotmail.com>
2024-02-27 19:52:56 -0300

    Revert "u"
    This reverts commit 4f3dcbb2b487b163f04c1665f574a68d9c0bdc3d.

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,9 +1,12 @@
-const { join } = require('path');
+import { fileURLToPath } from 'url';
+import path, { dirname, join } from 'path';
 
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const configuration = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
+
+export default configuration;
