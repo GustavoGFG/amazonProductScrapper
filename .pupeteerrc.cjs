207fec94469870f5dc7be10cd66becb062051175
Gustavo Fernandes <goncalves_017@hotmail.com>
2024-02-27 18:18:23 -0300

    a

---
+++ .pupeteerrc.cjs
@@ -0,0 +1,12 @@
+import { fileURLToPath } from 'url';
+import path, { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const configuration = {
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};
+
+export default configuration;
