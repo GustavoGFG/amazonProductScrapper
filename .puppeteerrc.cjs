37b9cc063f825402fa7c2abb66bfe40ec34a0d66
Gustavo Fernandes <goncalves_017@hotmail.com>
2024-02-27 21:00:40 -0300

    teste

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,12 +1,22 @@
-import { fileURLToPath } from 'url';
-import path, { dirname, join } from 'path';
+const { join } = require('path');
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const configuration = {
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
 
-export default configuration;
+// import { fileURLToPath } from 'url';
+// import path, { dirname, join } from 'path';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const configuration = {
+//   // Changes the cache location for Puppeteer.
+//   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+// };
+
+// export default configuration;
