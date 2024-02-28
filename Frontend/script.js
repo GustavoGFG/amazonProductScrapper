33ea01ca26916d6529b582590a3cfa610c917b23
Gustavo Fernandes <goncalves_017@hotmail.com>
2024-02-28 20:11:49 -0300

    :bug:Fix: changed backend url

--- Frontend/script.js
+++ Frontend/script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
       loadingAnimation.style.display = 'block';
       try {
         const response = await fetch(
-          `http://localhost:5000/api/scrape?keyword=${encodeURIComponent(
+          `https://amazonscrapperbackend.onrender.com//api/scrape?keyword=${encodeURIComponent(
             searchedWord
           )}`
         );
